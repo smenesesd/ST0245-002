0f43b6e05c2dfe1054a52ff05fba3cdb2ed909a3
npellegrib <68928357+npellegrib@users.noreply.github.com>
2020-08-15 22:42:52 -0500

    Create LeerArchivo.java

---
+++ proyecto/codigo/LeerArchivo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class FileReader {
+    public static void leerArchivo(String data){
+        ArrayList<String>  datos = new ArrayList<String>();
+        Scanner scan = new Scanner(System.in);
+        try{
+            File fichero = new File(data);
+            scan = new Scanner(fichero);
+            while (scan.hasNextLine()){
+                datos.add(scan.nextLine());
+            }
+        }catch (FileNotFoundException fnfe) {
+            System.out.println(fnfe);}
+        catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            scan.close();
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String data = scan.nextLine();
+        FileReader.leerArchivo(data);
+    }
+}
